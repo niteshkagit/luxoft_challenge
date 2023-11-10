b6025767a950d492eab91c218e4bc058c91bb74a
Nitesh Kashyap <nitesh.k.kashyap@oracle.com>
2023-11-10 23:13:43 +0530

    Review comment | taking single lock to do the transfer

--- src/main/java/com/dws/challenge/service/AccountBalanceServiceImpl.java
+++ src/main/java/com/dws/challenge/service/AccountBalanceServiceImpl.java
@@ -34,13 +34,13 @@ public class AccountBalanceServiceImpl implements AccountBalanceService{
         this.accountsRepository = accountsRepository;
     }
 
+    ReentrantLock lock = new ReentrantLock();
 
     /**
      * This method transfers balance between the accounts.
      * It also checks for some base condition which needs to meets and facilitate transfer
      *
-     * First take lock on source account id string and then verify the transfer condition(s)
-     * then take lock on destination account id and do the transfer
+     * First take lock and do the transfer between accounts
      *
      * @param srcAccountId
      * @param destAccountId
@@ -48,34 +48,23 @@ public class AccountBalanceServiceImpl implements AccountBalanceService{
      * @return returns true if transfer successful - else throw the exception
      */
     @Override
-    public boolean transferBalanceBWAccounts(String srcAccountId, String destAccountId, BigDecimal transferAmount) {
-        accountLocks.putIfAbsent(srcAccountId, new ReentrantLock()); // may consider using cache to have locks
-        accountLocks.putIfAbsent(destAccountId, new ReentrantLock());
-        ReentrantLock srcLock = accountLocks.get(srcAccountId);
-        ReentrantLock destLock = accountLocks.get(destAccountId);
+    public boolean  transferBalanceBWAccounts(String srcAccountId, String destAccountId, BigDecimal transferAmount) {
 
-        try {
-            srcLock.lock();
-            Account srcAccount = accountsRepository.getAccount(srcAccountId);
-            validateAccount(srcAccount, transferAmount,"SOURCE"); // We can have multiple such pre validations
-            try {
-                destLock.lock();
-                Account destAccount = accountsRepository.getAccount(destAccountId);
-                validateAccount(destAccount,transferAmount,"DEST");
-                destAccount.setBalance(destAccount.getBalance().add(transferAmount));
-                srcAccount.setBalance(srcAccount.getBalance().subtract(transferAmount));
-                notificationService.notifyAboutTransfer(srcAccount,"Dear account holder ! $"+ transferAmount +" is debited from account :"+srcAccountId);
-                notificationService.notifyAboutTransfer(destAccount,"Dear account holder ! $"+ transferAmount +" is credited from account :"+destAccountId+"\n");
-
-            } catch (Exception e) {
-                throw e;
-            } finally {
-                destLock.unlock();
-            }
-        } finally {
-            srcLock.unlock();
+        if (transferAmount.compareTo(new BigDecimal(0)) <= 0){
+            throw new InvalidTransactionException("Transfer amount can't be less than 0");
         }
+        Account srcAccount = accountsRepository.getAccount(srcAccountId);
+        Account destAccount = accountsRepository.getAccount(destAccountId);
+       try {
+           lock.lock(); // Making validate / credit / debit an atomic operation
+           validateAccount(srcAccount, transferAmount, "SOURCE"); // We can have multiple such pre validations
+           validateAccount(destAccount, transferAmount, "DEST");
+           srcAccount.setBalance(srcAccount.getBalance().subtract(transferAmount));
+           destAccount.setBalance(destAccount.getBalance().add(transferAmount));
 
+       }finally {
+           lock.unlock();
+       }
         return false;
     }
 
--- src/test/java/com/dws/challenge/service/AccountBalanceServiceImplTest.java
+++ src/test/java/com/dws/challenge/service/AccountBalanceServiceImplTest.java
@@ -9,9 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,8 +47,48 @@ class AccountBalanceServiceImplTest {
         assertThat(accountsService.getAccount("dest").getBalance()).isEqualByComparingTo("5500");
     }
 
+    /**
+     * Test to transfer $100 from source account to dest account using 2 threads , each thread having 100 iterations each
+     * @throws InterruptedException
+     */
     @Test
-    void transfer_multiple_transfers() throws InterruptedException {
+    void transfer_multiple_transfers() throws InterruptedException, ExecutionException {
+        accountsService.getAccountsRepository().clearAccounts();
+        Account account = new Account("source");
+        account.setBalance(new BigDecimal(90000));
+        this.accountsService.createAccount(account);
+
+        account = new Account("dest");
+        account.setBalance(new BigDecimal(20000));
+        this.accountsService.createAccount(account);
+        CountDownLatch latch = new CountDownLatch(2);
+        //Each runner job is doing 100 transaction
+        Callable runner = () -> {
+            for (int i = 0; i < 100 ; i++){
+                accountBalanceService.transferBalanceBWAccounts("source","dest",new BigDecimal(100));
+            }
+            latch.countDown();
+            return true;
+        };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Future task1 = executorService.submit(runner);
+        Future task2 = executorService.submit(runner);
+        latch.await();
+        assertThat(accountsService.getAccount("source").getBalance()).isEqualByComparingTo("70000");
+        assertThat(accountsService.getAccount("dest").getBalance()).isEqualByComparingTo("40000");
+    }
+
+
+    /**
+     * This test has 2 threads
+     * thread 1 transfers $100 from source to dest account [ 100 times ]
+     * thread2 transfers $100 from dest to source account [ 50 times ]
+     * @throws InterruptedException
+     */
+    @Test
+    void multiple_transfers_reverse_transfer() throws InterruptedException, ExecutionException {
         accountsService.getAccountsRepository().clearAccounts();
         Account account = new Account("source");
         account.setBalance(new BigDecimal(90000));
@@ -60,25 +98,31 @@ class AccountBalanceServiceImplTest {
         account.setBalance(new BigDecimal(20000));
         this.accountsService.createAccount(account);
 
-        //Each runner job is doing 100 transaction
-        Runnable runner = () -> {
+        //Each runner job is doing 100 transactions where it is transferring $100 from source account to dest account
+        Callable runner = () -> {
             for (int i = 0; i < 100 ; i++){
-                System.out.println(Thread.currentThread().getName() + ": transferring for : count "+i);
                 accountBalanceService.transferBalanceBWAccounts("source","dest",new BigDecimal(100));
             }
+            return true;
+        };
+        //Each runner job is doing 50 transactions where it is transferring $100 from dest account to source account
+        Callable reverseRunner = () -> {
+            for (int i = 0; i < 50 ; i++){
+                accountBalanceService.transferBalanceBWAccounts("dest","source",new BigDecimal(100));
+            }
+            return true;
         };
 
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        for(int i=0;i<2;i++){ // 2 threads forked
-            executorService.execute(runner);
-        }
-        executorService.shutdown();
-        boolean finished = executorService.awaitTermination(1, TimeUnit.MINUTES);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future task1 = executorService.submit(runner);
+        Future task2 = executorService.submit(reverseRunner);
 
-        assertThat(accountsService.getAccount("source").getBalance()).isEqualByComparingTo("70000");
-        assertThat(accountsService.getAccount("dest").getBalance()).isEqualByComparingTo("40000");
+        task2.get();
+        task1.get();
+
+        assertThat(accountsService.getAccount("source").getBalance()).isEqualByComparingTo("85000");
+        assertThat(accountsService.getAccount("dest").getBalance()).isEqualByComparingTo("25000");
     }
-
     @Test
     void transferBalanceBWAccounts_source_not_have_fund() throws InvalidTransactionException {
         accountsService.getAccountsRepository().clearAccounts();
@@ -94,25 +138,7 @@ class AccountBalanceServiceImplTest {
             accountBalanceService.transferBalanceBWAccounts("source_exceptional","dest_exceptional",new BigDecimal(4000));
         }catch (Exception e){
             assertThat(e instanceof InvalidTransactionException);
-            assertThat(e.getMessage()).isEqualTo("Not sufficient balance in account <source_exceptional>");
-        }
-    }
-
-    @Test
-    void transferBalanceBWAccounts_invalid_source_account() throws InvalidTransactionException {
-        accountsService.getAccountsRepository().clearAccounts();
-        Account account = new Account("source_exceptional");
-        account.setBalance(new BigDecimal(3000));
-        this.accountsService.createAccount(account);
-
-        account = new Account("dest_exceptional");
-        account.setBalance(new BigDecimal(1000));
-        this.accountsService.createAccount(account);
-
-        try {
-            accountBalanceService.transferBalanceBWAccounts("invalid_source_account","dest_exceptional",new BigDecimal(4000));
-        }catch (RuntimeException e){
-            assertThat(e.getMessage()).isEqualTo("invalid account !");
+            assertThat(e.getMessage()).contains("Not sufficient balance in account <source_exceptional>");
         }
     }
 
